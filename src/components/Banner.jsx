8eabd11afd95ee73ca231528289f9b181eb63b3e
José Felipe Alexandre Martins <felipealexandre178@gmail.com>
2024-08-07 12:18:29 -0300

    att: banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,12 +3,10 @@ import styles from './Banner.module.css';
 export default function Banner() {
   return (
     <section className={styles.banner}>
-      <div className={styles.text}>
-        <h1>BUSCAMOS ENTENDER O GOSTO DO CLIENTE</h1>
+      <div className={styles.content}>
+        <h1>BUSCAMOS ENTENDER O</h1> 
+        <h1>GOSTO DO CLIENTE</h1>
         <button>VER PROJETOS</button>
-      </div>
-      <div className={styles.image}>
-        {/* Sua imagem aqui */}
       </div>
     </section>
   );
