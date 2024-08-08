d5ff41abab0e64babc86468666186a40360129e5
E&E Technology <eetecnology1@gmail.com>
2024-08-08 18:41:48 -0300

    add Summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AboutMe from './components/AboutMe';
 import Banner from './components/Banner';
 import Header from './components/Header';
+import Summary from './components/Summary';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Banner />
       <AboutMe />
+      <Summary/>
     </div>
   )
 }
---
+++ src/components/Summary.jsx
@@ -0,0 +1,58 @@
+import styles from './Summary.module.css'
+
+export default function Summary() {
+    return (
+ <section className={styles.fund}>
+    <div className={styles.summary}>
+        <h2 className={styles.title}>
+        O QUE FAZEMOS
+        </h2>
+            <p className={styles.resume}>
+            Trabalhamos com o estilo contemporâneo e atemporal, 
+            onde buscamos entender o gosto do cliente e aplicar de <br /> 
+            forma personalizada, com um toque de decoração afetiva para ser sentida, 
+            com características únicas e sempre <br />
+            diferente uns dos outros, acreditamos que um lar deve refletir seus moradores.
+            </p>
+    </div>
+    <div className={styles.box}>
+        <div>
+            <h2>
+             ARQUITETURA
+            </h2>
+            <p>A empresa de design de interiores <br /> oferece 
+                um serviço <br /> completo, que atende às <br /> necessidades dos
+                clientes e <br /> cria projetos
+                que atendam <br /> aos seus sonhos.</p>
+        </div>
+        <div>
+             <h2>
+                DESIGN DE INTERIORES
+            </h2>
+            <p>
+                Planejamos o <br />desenvolvimento de 
+                projetos <br />para áreas residenciais e <br />comerciais
+                especificando <br /> itens necessários para que o <br />
+                ambiente fique completo.</p>
+        </div>
+        <div>
+            <h2>ASSESORIA DE OBRA</h2>
+            <p>
+                Tem caráter preventivo e <br /> busca conferir a qualidade <br />dos serviços,
+                atestando e <br />documentando a instalação <br /> do projeto executado. 
+                </p>
+        </div>
+        <div>
+            <h2>
+            CONSULTORIA DECORATIVA
+            </h2>
+            <p>
+                Para ambientes com a <br />arquitetura pronta que não <br />precisem de reforma,
+                apenas <br />melhorar o espaço de forma <br />rápida,
+                eficaz com itens de <br />decoração. 
+            </p>
+        </div>
+    </div>
+</section>
+    )
+}
