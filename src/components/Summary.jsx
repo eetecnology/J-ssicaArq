7f734466b54a0c39bbe65b74b54f99128b8ae252
E&E Technology <eetecnology1@gmail.com>
2024-08-09 22:33:11 -0300

    update animation and vars

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -23,56 +23,62 @@ export default function Summary() {
 
     return (
  <section className={styles.fund}>
+    <div className={styles.effectBox}>
     <div className={styles.summary}>
         <h2 className={styles.title}>
         O QUE FAZEMOS
         </h2>
             <p className={styles.resume}>
-                Trabalhamos com o estilo contemporâneo e atemporal, 
+                Trabalhamos com o <span className={styles.span}>estilo contemporâneo e atemporal</span>, 
                 onde buscamos entender o gosto do cliente e aplicar de <br /> 
                 forma personalizada, com um toque de decoração afetiva para ser sentida, 
-                com características únicas e sempre <br />
+                com <span className={styles.span}>características únicas</span> e sempre <br />
                 diferente uns dos outros, acreditamos que um lar deve refletir seus moradores.
             </p>
     </div>
     <div className={styles.box}>
         <div>
+            <img src="src/assets/detail.png" />
             <h2>
              ARQUITETURA
             </h2>
-            <p>A empresa de design de interiores <br /> oferece 
-                um serviço <br /> completo, que atende às <br /> necessidades dos
-                clientes e <br /> cria projetos
-                que atendam <br /> aos seus sonhos.</p>
+            <p>A empresa de design de interiores oferece 
+                um serviço  completo, que atende às necessidades dos
+                clientes e  cria projetos
+                que atendam  aos seus sonhos.</p>
         </div>
         <div>
+            <img src="src/assets/detail.png" />
              <h2>
                 DESIGN DE INTERIORES
             </h2>
             <p>
-                Planejamos o <br />desenvolvimento de 
-                projetos <br />para áreas residenciais e <br />comerciais
-                especificando <br /> itens necessários para que o <br />
+                Planejamos o desenvolvimento de 
+                projetos para áreas residenciais e comerciais
+                especificando  itens necessários para que o 
                 ambiente fique completo.</p>
         </div>
         <div>
-            <h2>ASSESORIA DE OBRA</h2>
+            <img src="src/assets/detail.png" />
+            <h2>ASSESORIAS DE OBRAS</h2>
             <p>
-                Tem caráter preventivo e <br /> busca conferir a qualidade <br />dos serviços,
-                atestando e <br />documentando a instalação <br /> do projeto executado. 
+                Tem caráter preventivo e  busca conferir a qualidade dos serviços,
+                atestando e documentando a instalação  do projeto executado. 
                 </p>
         </div>
         <div>
+            <img src="src/assets/detail.png" />
             <h2>
             CONSULTORIA DECORATIVA
             </h2>
             <p>
-                Para ambientes com a <br />arquitetura pronta que não <br />precisem de reforma,
-                apenas <br />melhorar o espaço de forma <br />rápida,
-                eficaz com itens de <br />decoração. 
+                Para ambientes com a arquitetura pronta que não precisem de reforma,
+                apenas melhorar o espaço de forma rápida,
+                eficaz com itens de decoração. 
             </p>
         </div>
     </div>
+</div>
 </section>
     )
 }
