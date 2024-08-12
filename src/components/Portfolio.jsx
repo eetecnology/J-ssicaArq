fbe4e457139233f8e8ddaf4b1eefcbad74a8c380
Netinho <netinhosp7@gmail.com>
2024-08-12 09:13:26 -0300

    att - Projects and Budjet

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,9 +9,9 @@ export default function Portfolio() {
         <p>Trabalhamos com o estilo contemporâneo e atemporal, onde buscamos entender o gosto do cliente e aplicar de forma personalizada, com um toque de decoração afetiva para ser sentida, com características únicas e sempre diferente uns dos outros, acreditamos que um lar deve refletir seus moradores.</p>
       </div>
       <div className={styles.projects}>
-        <Projects right={true} />
-        <Projects left={true} />
-        <Projects right={true} />
+        <Projects />
+        <Projects imageLeft={true} />
+        <Projects />
       </div>
     </section>
   )
