a83aa330e7c8602081ba1f736153500c09b9289c
José Felipe Alexandre Martins <felipealexandre178@gmail.com>
2024-08-12 10:00:55 -0300

    att: Projects imagem expande

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,25 @@
-import styles from './Projects.module.css'
+import styles from './Projects.module.css';
 import project1 from '../assets/project1.png';
 import project2 from '../assets/project2.png';
 import project3 from '../assets/project3.png';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export default function Projects({ imageLeft }) {
+  const [expandedImage, setExpandedImage] = useState(null);
+
   Projects.propTypes = {
     imageLeft: PropTypes.string,
   };
+
+  const handleImageClick = (imageSrc) => {
+    setExpandedImage(imageSrc);
+  };
+
+  const handleCloseClick = () => {
+    setExpandedImage(null);
+  };
+
   return (
     <div className={styles.projects}>
       <div className={`${styles.description} ${imageLeft ? styles.descriptionRight : ""}`}>
@@ -16,10 +28,31 @@ export default function Projects({ imageLeft }) {
         Transmitimos a essência dos clientes em nossos projetos, onde colocamos o nosso toque com a decoração afetiva da família que ali vai viver.</p>
       </div>
       <div className={`${styles.images} ${imageLeft ? styles.imageLeft : ""}`}>
-        <img src={project1} alt="Imagem do projeto" />
-        <img src={project2} alt="Imagem do projeto" />
-        <img src={project3} alt="Imagem do projeto" />
+        <img 
+          src={project1} 
+          alt="Imagem do projeto" 
+          className={styles.expandableImage}
+          onClick={() => handleImageClick(project1)}
+        />
+        <img 
+          src={project2} 
+          alt="Imagem do projeto" 
+          className={styles.expandableImage}
+          onClick={() => handleImageClick(project2)}
+        />
+        <img 
+          src={project3} 
+          alt="Imagem do projeto" 
+          className={styles.expandableImage}
+          onClick={() => handleImageClick(project3)}
+        />
       </div>
+      {expandedImage && (
+        <div className={styles.fullscreenImageContainer}>
+          <button className={styles.closeButton} onClick={handleCloseClick}>X</button>
+          <img src={expandedImage} alt="Imagem expandida" className={styles.fullscreenImage} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
