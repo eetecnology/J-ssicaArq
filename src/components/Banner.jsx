18623ead02a84bf89eddfe76486a77fb0a89e843
Netinho <netinhosp7@gmail.com>
2024-08-13 09:42:41 -0300

    att - Header

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,8 +12,7 @@ export default function Banner() {
   return (
     <section className={styles.banner}>
       <div className={styles.content}>
-        <h1>BUSCAMOS ENTENDER O</h1> 
-        <h1>GOSTO DO CLIENTE</h1>
+        <h1>BUSCAMOS ENTENDER O GOSTO DO CLIENTE</h1> 
         <Button className={styles.button} bgActive={true} onClick={handleWhatsAppClick}>
           VER PROJETOS
         </Button>
